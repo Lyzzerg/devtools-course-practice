d29d93e7f730715b359acdeb9495006423d0d772
Eugene Baranov <zcbw@mail.ru>
2017-05-08 00:07:09 +0300

    deleted test

--- modules/matrix-multiplication/test/test_matrix_multiplication.cpp
+++ modules/matrix-multiplication/test/test_matrix_multiplication.cpp
@@ -220,12 +220,3 @@ TEST(Baranov_Evgeny_MatrixMultiplicationTest, can_create_matrix_correct) {
     // Assert
     EXPECT_EQ(result, rows_columns);
 }
-
-TEST(Baranov_Evgeny_MatrixMultiplicationTest, can_create_matrix_correct) {
-    // Arrange
-    ALMatrix matrix(3, 4);
-    // Act
-	matrix[1][2] = "2";
-    // Assert
-    EXPECT_EQ("2", matrix[1][2]);
-}
